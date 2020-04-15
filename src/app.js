21340503774e86c10f7ef2df7ec6b61305856a7c
Fabricio Cerci <fabriciocerci@gmail.com>
2020-04-14 21:59:07 -0400

    Implement route that like a repository

--- src/app.js
+++ src/app.js
@@ -62,7 +62,13 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  repository.likes++;
+
+  return response.json(repository);
 });
 
 module.exports = app;
